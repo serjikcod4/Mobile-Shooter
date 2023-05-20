79d088346f383a22419a2198f8e43b48a1816854
serjikcod6 <sergeykushnir12@gmail.com>
2023-05-20 17:22:32 +0300

    Joystick + PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    public VariableJoystick variableJoystick;
-    public Rigidbody rb;
+    public FixedJoystick fixedJoystick;
 
-    public void FixedUpdate()
+    public void Update()
     {
-        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        Vector3 direction = Vector3.up * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal;
+        transform.Translate(direction * Time.deltaTime * speed);
     }
 }
